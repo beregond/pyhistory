91562abea0c322e2f09a1ceb00eabed47195c8bc
Szczepan Cieślik <szczepan.cieslik@amsterdam-standard.pl>
2014-06-20 16:33:56 +0200

    Moved commands, fixed error on clear.

--- pyhistory/cli.py
+++ pyhistory/cli.py
@@ -1,7 +1,7 @@
 import argparse
 
 from . import __version__ as ver
-from . import cli_commands
+from . import pyhistory
 
 parser = argparse.ArgumentParser(
     description="Manage Python project history file.")
@@ -13,10 +13,10 @@ subparsers = parser.add_subparsers(help="sub-command help")
 
 parser_add = subparsers.add_parser('add', help="add new message")
 parser_add.add_argument('message')
-parser_add.set_defaults(func=cli_commands.add)
+parser_add.set_defaults(func=pyhistory.add)
 
 parser_list = subparsers.add_parser('list', help="list actual history")
-parser_list.set_defaults(func=cli_commands.list_history)
+parser_list.set_defaults(func=pyhistory.list_history)
 
 parser_update = subparsers.add_parser(
     'update', help='update history file')
@@ -24,11 +24,11 @@ parser_update.add_argument('version')
 parser_update.add_argument(
     '--history-file', default='HISTORY.rst')
 parser_update.add_argument('--date', help="date of release (by default today)")
-parser_update.set_defaults(func=cli_commands.update)
+parser_update.set_defaults(func=pyhistory.update)
 
 parser_clear = subparsers.add_parser(
     'clear', help="remove entries from history directory")
-parser_clear.set_defaults(func=cli_commands.clear)
+parser_clear.set_defaults(func=pyhistory.clear)
 
 
 def main():
--- pyhistory/pyhistory.py
+++ pyhistory/pyhistory.py
@@ -1,1 +1,76 @@
-# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os.path
+import shutil
+from datetime import date
+from hashlib import md5
+
+
+def add(args):
+    _check_history_dir(args.history_dir)
+    hashed = md5(args.message).hexdigest()[:7]
+
+    filepath = os.path.join(args.history_dir, hashed)
+    if os.path.exists(filepath):
+        raise RuntimeError("Given entry already exists!")
+
+    with open(filepath, 'w') as entry_file:
+        entry_file.write(args.message + '\n')
+
+
+def list_history(args):
+    lines = _list_history(args.history_dir)
+    print('\n' + ''.join(lines))
+
+
+def update(args):
+    with open(args.history_file) as history_file:
+        lines = history_file.readlines()
+
+    result = lines[:3]
+
+    release_date = args.date or date.today().strftime('%Y-%m-%d')
+    header = '{} ({})'.format(args.version, release_date)
+    result.append(header + '\n')
+    result.append('+' * len(header) + '\n\n')
+
+    result += _list_history(args.history_dir)
+    result.append('\n')
+
+    result += lines[3:]
+
+    result = ''.join(result)
+
+    with open(args.history_file, 'w') as history_file:
+        history_file.write(result)
+
+    _delete_history_dir(args.history_dir)
+
+
+def clear(args):
+    _delete_history_dir(args.history_dir)
+
+
+def _list_history(history_dir):
+    result = []
+    for root, _, files in os.walk(history_dir):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            with open(fullpath) as file_handler:
+                result.append('* ' + file_handler.read())
+
+    return result
+
+
+def _check_history_dir(history_dir):
+    try:
+        os.stat(history_dir)
+    except OSError:
+        os.mkdir(history_dir)
+
+
+def _delete_history_dir(history_dir):
+    try:
+        shutil.rmtree(history_dir)
+    except OSError:
+        pass
