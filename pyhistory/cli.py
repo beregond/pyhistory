f621d3b86e639b8e7a815fbc92fc8ace5a5d824a
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2014-06-30 19:44:09 +0200

    Upgraded codebase.

--- pyhistory/cli.py
+++ pyhistory/cli.py
@@ -18,21 +18,16 @@ parser_add.set_defaults(func=pyhistory.add)
 parser_list = subparsers.add_parser('list', help="list actual history")
 parser_list.set_defaults(func=pyhistory.list_history)
 
-parser_update = subparsers.add_parser(
-    'update', help='update history file')
-parser_update.add_argument('version')
-parser_update.add_argument(
-    '--history-file', default='HISTORY.rst')
-parser_update.add_argument('--date', help="date of release (by default today)")
-parser_update.set_defaults(func=pyhistory.update)
+update_parsers = [
+    subparsers.add_parser('update', help='update history file'),
+    subparsers.add_parser('squash', help='alias to "update"'),
+]
 
-parser_squash = subparsers.add_parser(
-    'squash', help='update history file (alias to update)')
-parser_squash.add_argument('version')
-parser_squash.add_argument(
-    '--history-file', default='HISTORY.rst')
-parser_squash.add_argument('--date', help="date of release (by default today)")
-parser_squash.set_defaults(func=pyhistory.update)
+for uparser in update_parsers:
+    uparser.add_argument('version')
+    uparser.add_argument('--history-file', default='HISTORY.rst')
+    uparser.add_argument('--date', help="date of release (by default today)")
+    uparser.set_defaults(func=pyhistory.update)
 
 parser_clear = subparsers.add_parser(
     'clear', help="remove entries from history directory")
