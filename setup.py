ae0fa6f4272123d9c4efc4fd771bc9cef86b4047
Szczepan Cieślik <szczepan.cieslik@gmail.com>
2022-04-04 17:41:25 +0200

    Update supported python version

--- setup.py
+++ setup.py
@@ -50,8 +50,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'click',
-        'pathlib',
-        'six',
     ],
     entry_points={
         'console_scripts': [
@@ -66,13 +64,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     cmdclass={
         'test': PyTest,
